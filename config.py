b3dd9291b2b853ee9be35533eceb8d4d916922cb
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2024-09-17 07:23:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "7236453")
     API_HASH = os.environ.get("API_HASH", "33010a70e94f80e55145980072cce969")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7166294138:AAGxFySdLXhK6hA7NZs2rHF3A98Kl-Ub7IM") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7219591797:AAFaq-VwCbRiTbGy0MuRce6nxrn7yUh2tjg") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://madtoazenzio:f9oDLc4c6H5zdP44@devutty.pk5so.mongodb.net/?retryWrites=true&w=majority&appName=devutty")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
